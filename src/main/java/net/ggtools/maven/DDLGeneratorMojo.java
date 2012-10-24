9190107f5b0d04574a031a5c869f5783cd5f8d55
Christophe Labouisse <christophe@labouisse.org>
2012-10-24 12:36:00 +0200

    Update copyright

--- src/main/java/net/ggtools/maven/DDLGeneratorMojo.java
+++ src/main/java/net/ggtools/maven/DDLGeneratorMojo.java
@@ -1,7 +1,10 @@
 /*
  * This file is part of ddlGenerator. Copyright ©2012 Christophe Labouisse
  *
- * ddlGenerator is free software: you can redistribute it and/or modifyit under the terms of the GNU General Public License as published bythe Free Software Foundation, either version 3 of the License, or(at your option) any later version.
+ * ddlGenerator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * ddlGenerator is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,22 +16,6 @@
  */
 
 package net.ggtools.maven;
-
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import static net.ggtools.maven.ddlgenerator.SpringConfiguration.ENV_PREFIX;
 
--- src/main/java/net/ggtools/maven/ddlgenerator/SpringConfiguration.java
+++ src/main/java/net/ggtools/maven/ddlgenerator/SpringConfiguration.java
@@ -1,7 +1,10 @@
 /*
  * This file is part of ddlGenerator. Copyright ©2012 Christophe Labouisse
  *
- * ddlGenerator is free software: you can redistribute it and/or modifyit under the terms of the GNU General Public License as published bythe Free Software Foundation, either version 3 of the License, or(at your option) any later version.
+ * ddlGenerator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * ddlGenerator is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
