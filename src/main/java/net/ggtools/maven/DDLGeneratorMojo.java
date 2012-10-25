ed4be977be8125f60b2b78e9a4561023abc2bfa3
Christophe Labouisse <christophe@labouisse.org>
2012-10-26 00:47:44 +0200

    Changed javadoc annotations by Java5 Annotations. Fixed goal & default phase.

--- src/main/java/net/ggtools/maven/DDLGeneratorMojo.java
+++ src/main/java/net/ggtools/maven/DDLGeneratorMojo.java
@@ -24,6 +24,9 @@ import net.ggtools.maven.ddlgenerator.DDLGenerator;
 import net.ggtools.maven.ddlgenerator.SpringConfiguration;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
@@ -36,95 +39,83 @@ import java.util.Map;
 import static net.ggtools.maven.ddlgenerator.SpringConfiguration.ENV_PREFIX;
 
 /**
- * Goal which touches a timestamp file. 
- * 
- * @goal touch
- * @phase process-sources
+ * Create a SQL file from an JPA/Hibernat project.
  */
+@Mojo(defaultPhase = LifecyclePhase.PREPARE_PACKAGE, name = "generate")
 @SuppressWarnings("unused")
 @Setter(AccessLevel.PACKAGE)
 @Getter(AccessLevel.PACKAGE)
 public class DDLGeneratorMojo extends AbstractMojo {
 
-	/**
-	 * Destination file
-	 * 
-	 * @parameter
-	 * @required
-	 */
-	private File ddlFile;
+    /**
+     * Destination file
+     */
+    @Parameter(required = true)
+    private File ddlFile;
 
-	/**
-	 * Default schema
-	 * 
-	 * @parameter
-	 */
-	private String defaultSchema;
+    /**
+     * Default schema
+     */
+    @Parameter(required = false)
+    private String defaultSchema;
 
-	/**
-	 * Database dialect.
-	 * 
-	 * @parameter
-	 * @required
-	 */
-	private String dialect;
+    /**
+     * Database dialect.
+     */
+    @Parameter(required = true)
+    private String dialect;
 
-	/**
-	 * Naming strategy.
-	 * 
-	 * @parameter
-	 */
-	private String namingStrategy;
+    /**
+     * Naming strategy.
+     */
+    @Parameter(required = false)
+    private String namingStrategy;
 
-	/**
-	 * Name of the persistence unit used for the DDL generation.
-	 * 
-	 * @parameter
-	 * @required
-	 */
-	private String persistenceUnitName;
+    /**
+     * Name of the persistence unit used for the DDL generation.
+     */
+    @Parameter(required = true)
+    private String persistenceUnitName;
 
-	/**
-	 * Locations of the persistence.xml files.
-	 * 
-	 * @parameter
-	 */
-	private String[] persistenceXmlLocations;
+    /**
+     * Locations of the persistence.xml files.
+     */
+    @Parameter(required = false)
+    private String[] persistenceXmlLocations;
 
-	/**
-	 * Whether or not use the new generator mapping in hibernate
-	 * 
-	 * @parameter default-value=false
-	 */
-	private Boolean useNewGenerator;
+    /**
+     * Whether or not use the new generator mapping in hibernate
+     */
+    @Parameter(required = false, defaultValue = "false")
+    private Boolean useNewGenerator;
 
-	AnnotationConfigApplicationContext createApplicationContext() {
-		final AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
-		final ConfigurableEnvironment environment = applicationContext.getEnvironment();
-		final MutablePropertySources propertySources = environment.getPropertySources();
-		final MapPropertySource propertySource = createPropertySource();
-		propertySources.addFirst(propertySource);
-		applicationContext.register(SpringConfiguration.class);
-		applicationContext.refresh();
-		return applicationContext;
-	}
+    AnnotationConfigApplicationContext createApplicationContext() {
+        final AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        final ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        final MutablePropertySources propertySources = environment.getPropertySources();
+        final MapPropertySource propertySource = createPropertySource();
+        propertySources.addFirst(propertySource);
+        applicationContext.register(SpringConfiguration.class);
+        applicationContext.refresh();
+        return applicationContext;
+    }
 
-	MapPropertySource createPropertySource() {
-		final Map<String, Object> properties = new HashMap<String, Object>();
-		properties.put(ENV_PREFIX + ".ddlFile", ddlFile);
-		properties.put(ENV_PREFIX + ".defaultSchema", defaultSchema);
-		properties.put(ENV_PREFIX + ".dialect", dialect);
-		properties.put(ENV_PREFIX + ".namingStrategy", namingStrategy);
-		properties.put(ENV_PREFIX + ".persistenceUnitName", persistenceUnitName);
-		properties.put(ENV_PREFIX + ".useNewGenerator", useNewGenerator);
-		properties.put(ENV_PREFIX + ".persistenceXmlLocations", persistenceXmlLocations);
+    MapPropertySource createPropertySource() {
+        final Map<String, Object> properties = new HashMap<String, Object>();
+        properties.put(ENV_PREFIX + ".ddlFile", ddlFile);
+        properties.put(ENV_PREFIX + ".defaultSchema", defaultSchema);
+        properties.put(ENV_PREFIX + ".dialect", dialect);
+        properties.put(ENV_PREFIX + ".namingStrategy", namingStrategy);
+        properties.put(ENV_PREFIX + ".persistenceUnitName", persistenceUnitName);
+        properties.put(ENV_PREFIX + ".useNewGenerator", useNewGenerator);
+        properties.put(ENV_PREFIX + ".persistenceXmlLocations", persistenceXmlLocations);
         properties.put(ENV_PREFIX + ".log", getLog());
-		return new MapPropertySource("mojoPropertySource", properties);
-	}
+        return new MapPropertySource("mojoPropertySource", properties);
+    }
 
-	@Override
-	public void execute() throws MojoExecutionException {
-		AnnotationConfigApplicationContext applicationContext = null;
+    @Override
+    public void execute() throws MojoExecutionException {
+        AnnotationConfigApplicationContext applicationContext = null;
 
         try {
             applicationContext = createApplicationContext();
