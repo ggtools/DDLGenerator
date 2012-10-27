c374ee9d0cf5a97d28bcd6490b24c4d1ce1257a8
Christophe Labouisse <christophe@labouisse.org>
2012-10-27 16:18:53 +0200

    Added a sample with multiple projects.

---
+++ samples/multiProject/domain/src/main/java/net/ggtools/maven/ddlgenerator/samples/multiproject/domain/DomainEntity.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of ddlGenerator. Copyright ©2012 Christophe Labouisse
+ *
+ * ddlGenerator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ddlGenerator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ddlGenerator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.ggtools.maven.ddlgenerator.samples.multiproject.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: Christophe Labouisse
+ * Date: 10/25/12
+ * Time: 15:30
+ */
+@Entity
+@Data
+public class DomainEntity {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column
+    private String domainName;
+
+    @ElementCollection
+    private List<String> selection = new ArrayList<String>();
+}
